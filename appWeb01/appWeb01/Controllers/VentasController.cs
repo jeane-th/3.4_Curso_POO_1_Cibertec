bc859f64eebb263bea11098a95d4ba6436355e63
Jeaneth Tafur <jeanethtafur@gmail.com>
2026-02-16 12:16:28 -0500

    Laboratorio 1.1

---
+++ appWeb01/appWeb01/Controllers/VentasController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using appWeb01.Models;
+
+
+namespace appWeb01.Controllers
+{
+    public class VentasController : Controller
+    {
+        // GET: Ventas 
+        public ActionResult ActionVenta()
+        {
+            // inicialización de los valores de la vista
+            // Hay q inicializar los valores para evital que salga null al cargar la pagina
+            ViewBag.Descuento = 0;
+            ViewBag.Total = 0;
+            // Instanciación del Modelo
+            return View(new Venta());
+        }
+        [HttpPost]public ActionResult ActionVenta(Venta reg)
+        {
+            ViewBag.Descuento = reg.Descuento();
+            ViewBag.Total = reg.Total();
+            return View(reg);
+        }
+    }
+}
---
+++ appWeb01/appWeb01/Models/Venta.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace appWeb01.Models
+{
+    public class Venta
+    {   
+        // Atributos
+        public int Boleta { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Fecha { get; set; } = DateTime.Today;
+        public string Nombre { get; set; }
+        public string Mueble { get; set; }
+        public double Precio { get; set; }
+
+        // Constructor
+        public Venta() { }
+
+        // Metodos
+        public double Descuento()
+        {
+            if (Precio>500) 
+            {
+                return Precio * 0.1;
+            }
+            return Precio*0.02;
+
+        }
+
+        public double Total()
+            { return Precio - Descuento(); }
+
+    }
+
+}
